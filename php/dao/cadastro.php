a78974d318b7b4564e511f54185c7d2314cadd5b
Rafael Teixeira Miguel <teixeirarafael12@gmail.com>
2019-12-05 21:21:45 -0300

    Sign Up

--- php/dao/cadastro.php
+++ php/dao/cadastro.php
@@ -1,7 +1,7 @@
 <?php
 
-$servername = "127.0.0.1:8080";
-$database = "cquiz";
+$servername = "127.0.0.1";
+$database = "quiz";
 $username = "root";
 $password = "usbw";
 // Create connection
@@ -24,13 +24,14 @@ $sexo = $_POST['sexo'];
 $senha = $_POST['senha'];
 
 
-$sql = "INSERT INTO usuario (nome, email, cpf, dataNascimento, sexo, telefone, senha) 
-    VALUES($nome, $email, $cpf, $dataNascimento, $sexo, $telefone, $senha)";
+$sql = "INSERT INTO usuario (id,nome, email, cpf, dataNasc, sexo, telefone, senha) 
+    VALUES(3,'$nome', '$email', '$cpf', STR_TO_DATE('$dataNascimento','%Y-%m-%d'), '$sexo', '$telefone', '$senha')";
 if (mysqli_query($conn, $sql)) {
       echo "Cadastrado com sucesso";
+      header("Location: http://localhost/FATEC/PPI/P2/php/home.php"); 
 } else {
       echo "Error ao cadastrar: " . $sql . "<br>" . mysqli_error($conn);
+      echo "<br/><a href='http://localhost/FATEC/PPI/P2/php/home.php'>Home</a>";
 }
 mysqli_close($conn);
-
 ?>
